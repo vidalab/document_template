8fe14b2678ddf38ccbd22408e4a22ef1137003f6
Phuoc Do <dnprock@gmail.com>
2015-04-07 23:47:40 -0700

    Add support for loading multiple data files

--- vida_template.js
+++ vida_template.js
@@ -9,15 +9,12 @@ $(document).ready(function() {
     
     // load HTML and append to body canvas
     var document_html = manifest.html[0],
-        document_css = manifest.stylesheet[0],
-        document_data = manifest.data[0]
+        document_css = manifest.stylesheet[0]
       
     $.get(document_html, function(html) {
       $('#canvas').append(html);
       
-      $.get(document_data, function(data) {
-        window.data = data;
-
+      function run() {
         // load CSS
         var css = $('<link rel="stylesheet" type="text/css" href="' + document_css + '" />');
         $('body').append(css);
@@ -28,7 +25,34 @@ $(document).ready(function() {
           var script = $('<script type="text/javascript" src="' + js +'"></script>');
           $('body').append(script);
         }
-      })
+      }
+      
+      function loadData(i) {
+        if (i < manifest.data.length) {
+          var document_data = manifest.data[i]
+          if (document_data.indexOf('.json') !== -1) {
+            $.get(document_data, function(data) {
+              window.data[document_data] = data;
+              loadData(i+1);
+            })
+          } else if (document_data.indexOf('.csv') !== -1) {
+            d3.csv(document_data, function(data) {
+              window.data[document_data] = data;
+              loadData(i+1);
+            });
+          } else if (document_data.indexOf('.tsv') !== -1) {
+            d3.tsv(document_data, function(data) {
+              window.data[document_data] = data;
+              loadData(i+1);
+            });
+          }
+        } else {
+          run();
+        }
+      }
+      
+      window.data = {};
+      loadData(0);
     });
   });
   
