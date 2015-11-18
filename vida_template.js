10bb8cb697f4f9a8c431e8244874c0920026b739
Phuoc Do <dnprock@gmail.com>
2015-11-18 09:55:43 -0800

    Load external lib in order

--- vida_template.js
+++ vida_template.js
@@ -7,61 +7,73 @@ $(document).ready(function() {
       window.config[p.name] = p.value;
     });
 
-    if (manifest.externalLibraries) {
-      for (var i = 0; i < manifest.externalLibraries.length; i++) {
-        var js = manifest.externalLibraries[i];
-        var script = $('<script type="text/javascript" src="' + js +'"></script>');
-        $('body').append(script);
-      }
+    function loadDoc() {
+      // load HTML and append to body canvas
+      var document_html = manifest.html[0],
+          document_css = manifest.stylesheet[0]
+
+      $.get(document_html, function(html) {
+        $('#canvas').append(html);
+
+        function run() {
+          // load CSS
+          var css = $('<link rel="stylesheet" type="text/css" href="' + document_css + '" />');
+          $('body').append(css);
+
+          // load Javascript
+          for (var i = 0; i < manifest.javascript.length; i++) {
+            var js = manifest.javascript[i];
+            var script = $('<script type="text/javascript" src="' + js +'"></script>');
+            $('body').append(script);
+          }
+        }
+
+        function loadData(i) {
+          if (i < manifest.data.length) {
+            var document_data = manifest.data[i]
+            if (document_data.indexOf('.json') !== -1) {
+              $.get(document_data, function(data) {
+                window.data[document_data] = data;
+                loadData(i+1);
+              })
+            } else if (document_data.indexOf('.csv') !== -1) {
+              d3.csv(document_data, function(data) {
+                window.data[document_data] = data;
+                loadData(i+1);
+              });
+            } else if (document_data.indexOf('.tsv') !== -1) {
+              d3.tsv(document_data, function(data) {
+                window.data[document_data] = data;
+                loadData(i+1);
+              });
+            }
+          } else {
+            run();
+          }
+        }
+
+        window.data = {};
+        loadData(0);
+      });
     }
 
-    // load HTML and append to body canvas
-    var document_html = manifest.html[0],
-        document_css = manifest.stylesheet[0]
-
-    $.get(document_html, function(html) {
-      $('#canvas').append(html);
-
-      function run() {
-        // load CSS
-        var css = $('<link rel="stylesheet" type="text/css" href="' + document_css + '" />');
-        $('body').append(css);
-
-        // load Javascript
-        for (var i = 0; i < manifest.javascript.length; i++) {
-          var js = manifest.javascript[i];
-          var script = $('<script type="text/javascript" src="' + js +'"></script>');
-          $('body').append(script);
-        }
-      }
-
-      function loadData(i) {
-        if (i < manifest.data.length) {
-          var document_data = manifest.data[i]
-          if (document_data.indexOf('.json') !== -1) {
-            $.get(document_data, function(data) {
-              window.data[document_data] = data;
-              loadData(i+1);
-            })
-          } else if (document_data.indexOf('.csv') !== -1) {
-            d3.csv(document_data, function(data) {
-              window.data[document_data] = data;
-              loadData(i+1);
-            });
-          } else if (document_data.indexOf('.tsv') !== -1) {
-            d3.tsv(document_data, function(data) {
-              window.data[document_data] = data;
-              loadData(i+1);
-            });
-          }
+    if (manifest.externalLibraries && manifest.externalLibraries.length > 0) {
+      function loadExtLib(index) {
+        if (index < manifest.externalLibraries.length) {
+          var js = manifest.externalLibraries[index];
+          $.getScript(js, function() {
+            index++;
+            loadExtLib(index);
+          });
         } else {
-          run();
+          loadDoc();
         }
       }
 
-      window.data = {};
-      loadData(0);
-    });
+      loadExtLib(0);
+    } else {
+      loadDoc();
+    }
   });
 
 });
